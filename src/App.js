7841deec2b5311490f65ba1254759f5bdf437d96
OlexandrVaidych <10lexandr.vaidych@gmail.com>
2023-08-28 10:43:25 +0300

    input created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [name, setName] = useState('');
+
+    const changeName = (event) => {
+        setName(event.target.value);
+    }
+
+    const showName = () => {
+        console.log(name);
+    }
+
   return (
     <div className="App">
       <header>
         <h1>Ukrainian Cities</h1>
       </header>
+        <div>
+            Enter city name:<br/>
+            <input
+                type="text"
+                onChange={changeName}
+            /><br/>
+            <button onClick={showName}>Show information</button>
+        </div>
     </div>
   );
 }
