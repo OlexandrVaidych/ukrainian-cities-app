15553353fabd720b113b8534540dda2cd800145c
OlexandrVaidych <10lexandr.vaidych@gmail.com>
2023-08-31 11:55:08 +0300

    images added

--- src/City.js
+++ src/City.js
@@ -1,9 +1,12 @@
+import "./City.css"
+
 function City({nameCity, cityData}){
     return (
         <div>
             {cityData[nameCity] ? (
                     <div>
-                        <p>Name: {cityData[nameCity].name}</p>
+                        <p>{cityData[nameCity].name}</p>
+                        <img src={cityData[nameCity].img} alt="City image" className="Image"/>
                         <p>Description: {cityData[nameCity].description}</p>
                         <p>Population: {cityData[nameCity].population}</p>
                     </div>
